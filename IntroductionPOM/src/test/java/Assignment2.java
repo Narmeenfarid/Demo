556d649786af71bd273ea0e9daf88fcd970c7b0b
Narmeen <Narmeen@DESKTOP-UNCQVLA>
2021-12-31 13:19:55 +0500

    Assignment2 completed

--- IntroductionPOM/src/test/java/Assignment2.java
+++ IntroductionPOM/src/test/java/Assignment2.java
@@ -15,23 +15,50 @@ public class Assignment2 {
 		ChromeDriver driver= new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://me.cleartrip.com/");
-		driver.findElement(By.cssSelector("input[class='field bw-1 bs-solid w-100p p-2 box-border br-4 fs-2 c-neutral-900 h-8 bc-neutral-100 c-neutral-900 focus:bc-secondary-500 flex flex-middle flex-between t-all fs-2 focus:bc-secondary-500 bg-transparent bc-neutral-100 pr-2 pl-3 pt-2 pb-2 ba br-4 h-8 null']")).sendKeys("New");
+		
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scrollBy(0,300)", "");
 		Thread.sleep(2000);
-		List<WebElement> options=driver.findElements(By.xpath("(//li[@class='ls-reset br-4 w-100p px-2 dropdown__item c-neutral-900 fs-3 hover:bg-secondary-500 hover:c-white c-pointer'])"));
+		driver.findElement(By.cssSelector("input[class='field bw-1 bs-solid w-100p p-2 box-border br-4 fs-2 c-neutral-900 h-8 bc-neutral-100 c-neutral-900 focus:bc-secondary-500 flex flex-middle flex-between t-all fs-2 focus:bc-secondary-500 bg-transparent bc-neutral-100 pr-2 pl-3 pt-2 pb-2 ba br-4 h-8 null']")).sendKeys("New");
+		Thread.sleep(8000);
+		List<WebElement> options=driver.findElements(By.xpath("//li[@class='ls-reset br-4 w-100p px-2 dropdown__item c-neutral-900 fs-3 hover:bg-secondary-500 hover:c-white c-pointer']"));
+		
+		
 		for(WebElement option: options) {
-			// loop will run and mje options me sy ek option chahie every time when loop iterates and return type
-			//of that element will be WebElement
+	
 			if(option.getText().equalsIgnoreCase("New York, US - All airports (NYC)")){
 				option.click();
 				break;
 			
 			}
-			JavascriptExecutor js = (JavascriptExecutor) driver;
-			js.executeScript("window.scrollBy(0,300)", "");
+		}
+				
+				
+				
+	
+driver.findElement(By.cssSelector("button[class='flex flex-middle flex-between t-all fs-2 focus:bc-secondary-500 bg-transparent bc-neutral-100 c-pointer pr-2 pl-3 pt-2 pb-2 ba br-4 h-8 c-neutral-900']")).click();
+
+driver.findElement(By.xpath("//div[contains(@class,'DayPicker-Day--today')][1]")).click();
 			WebElement adult= driver.findElement(By.cssSelector("div[class='mb-4'] select[class='bc-neutral-100 bg-transparent']"));
 			Select s =new Select(adult);
-			s.selectByValue("2");
+			s.selectByValue("3");
+			
+			WebElement child= driver.findElement(By.xpath("(//select[@class='bc-neutral-100 bg-transparent'])[2]"));
+			Select s1 =new Select(child);
+			s1.selectByValue("1");
+			
+			
+			WebElement infant= driver.findElement(By.xpath("(//select[@class='bc-neutral-100 bg-transparent'])[3]"));
+			Select s2 =new Select(infant);
+			s2.selectByValue("3");
+		
+			driver.findElement(By.xpath("//button[normalize-space()='Search flights']")).click();
+			
+			String message=driver.findElement(By.xpath("//span[@class='to-ellipsis o-hidden ws-nowrap fs-3 c-white']")).getText();
+			
+			System.out.println("The message is " + message);
+//			
 		}
 	}
 
-}
+
