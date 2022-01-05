055e97587b1479f49f88d62cfc3e48b1840ad3d1
Narmeen <Narmeen@DESKTOP-UNCQVLA>
2022-01-05 12:46:39 +0500

    Scope

---
+++ IntroductionPOM/src/test/java/Scope.java
@@ -0,0 +1,46 @@
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Scope {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\Narmeen\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		// driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("https://qaclickacademy.com/");
+		WebDriverWait wait = new WebDriverWait(driver, 20);
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='sumome-react-svg-image-container']")));
+
+		// driver.findElement(By.xpath("//div[@class='sumome-react-svg-image-container']"));
+		Actions a = new Actions(driver);
+		a.moveToElement(driver.findElement(By.xpath("//div[@class='sumome-react-svg-image-container']"))).click()
+				.build().perform();
+
+		driver.findElement(By.linkText("PRACTICE")).click();
+		
+		System.out.println(driver.findElements(By.tagName("a")).size());
+		WebElement footerdriver=driver.findElement(By.id("gf-BIG"));
+		System.out.println(footerdriver.findElements(By.tagName("a")).size());
+		WebElement columndriver=footerdriver.findElement(By.xpath("//tbody/tr/td[1]/ul[1]"));
+		System.out.println(columndriver.findElements(By.tagName("a")).size());
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scrollBy(0,2000)");
+		for(int i=1;i<columndriver.findElements(By.tagName("a")).size();i++) {
+			String clickonlink=Keys.chord(Keys.CONTROL,Keys.ENTER);
+			columndriver.findElements(By.tagName("a")).get(i).sendKeys(clickonlink);
+		}
+
+	}
+
+}
